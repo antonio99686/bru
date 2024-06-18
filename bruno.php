7ad17322d48eebe9bec8a8609274f1407b534c47
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-06-17 21:21:27 -0300

    bee

---
+++ bruno.php
@@ -0,0 +1,12 @@
+<?php
+// Define o tempo de expiração do cookie (30 segundos)
+$bruno = time() + 30;
+
+// Verifica se o formulário foi enviado e se o nome foi preenchido
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['nome'])){
+    // define o cookie com o nome do usuario
+    setcookie(username)
+
+}
+
+?>
--- index.php
+++
@@ -1,2 +0,0 @@
-Um formulário para o usuário inserir seu nome.
-Um script PHP que salva o nome do usuário em um cookie e exibe uma mensagem de boas-vindas com base no valor do cookie.
